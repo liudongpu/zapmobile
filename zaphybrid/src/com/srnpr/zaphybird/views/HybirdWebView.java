edcde3556475f4da776b82310da1228fbeea7bc7
srnpr <srnpr@srnprmacmini>
2014-07-06 16:24:17 +0800

    add zaphybird

---
+++ zaphybrid/src/com/srnpr/zaphybird/views/HybirdWebView.java
@@ -0,0 +1,14 @@
+package com.srnpr.zaphybird.views;
+
+import android.content.Context;
+import android.webkit.WebView;
+
+public class HybirdWebView  extends WebView{
+
+	public HybirdWebView(Context context) {
+		
+		super(context);
+		
+	}
+
+}
