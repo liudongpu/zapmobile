2d5348d8d60e2a213de00c70f15d343803ca3d28
srnpr <srnpr@srnprmacmini>
2014-07-06 14:45:51 +0800

    修正切换tab时的显示问题

--- doctor/src/com/srnpr/zapmobile/doctor/FragmentPage1.java
+++ doctor/src/com/srnpr/zapmobile/doctor/FragmentPage1.java
@@ -9,6 +9,10 @@ import android.webkit.WebView;
 
 public class FragmentPage1 extends Fragment {
 
+	
+	 private View rootView;//缓存Fragment view  
+	
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -16,8 +20,28 @@ public class FragmentPage1 extends Fragment {
 		// WebView wv=(WebView) container.findViewById(R.id.wv);
 
 		// wv.loadUrl("http://www.baidu.com");
-
-		return inflater.inflate(R.layout.fragment_1, null);
+		//return inflater.inflate(R.layout.fragment_1, null);
+		
+		
+		 if(rootView==null){  
+	            rootView=inflater.inflate(R.layout.fragment_1, null);  
+	            
+	            
+	            WebView webview = (WebView) rootView.findViewById(R.id.wv);
+	    		
+	    		webview.getSettings().setJavaScriptEnabled(true);
+	    		
+	    		webview.loadUrl("http://www.baidu.com/");
+	            
+	            
+	        }  
+	 //缓存的rootView需要判断是否已经被加过parent， 如果有parent需要从parent删除，要不然会发生这个rootview已经有parent的错误。  
+	        ViewGroup parent = (ViewGroup) rootView.getParent();  
+	        if (parent != null) {  
+	            parent.removeView(rootView);  
+	        }   
+	        return rootView;  
+		
 	}
 
 	
@@ -26,18 +50,12 @@ public class FragmentPage1 extends Fragment {
 	public void onActivityCreated(Bundle savedInstanceState) {
 
 		
+		
 		super.onActivityCreated(savedInstanceState);
-	
 			
 
-			
-			WebView webview = (WebView) this.getView().findViewById(R.id.wv);
-			
-			webview.getSettings().setJavaScriptEnabled(true);
-			
-			webview.loadUrl("http://www.baidu.com/");
-			
 
+		
 		
 
 	}
