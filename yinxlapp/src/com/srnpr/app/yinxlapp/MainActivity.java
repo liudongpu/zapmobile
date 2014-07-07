3e304f36d26ead61e83c55f92db244db9bd04a0e
srnpr <srnpr@srnprmacmini>
2014-07-07 23:07:29 +0800

    a

--- yinxlapp/src/com/srnpr/app/yinxlapp/MainActivity.java
+++ yinxlapp/src/com/srnpr/app/yinxlapp/MainActivity.java
@@ -1,22 +1,53 @@
 package com.srnpr.app.yinxlapp;
 
+import org.apache.cordova.Config;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import com.srnpr.zaphybird.views.HybirdActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
 
-public class MainActivity extends Activity {
+public class MainActivity extends HybirdActivity  implements CordovaInterface{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		//CordovaWebView cwv = (CordovaWebView) findViewById(R.id.text1);
+		Config.init(this);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Override
+	public void setActivityResultCallback(CordovaPlugin arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void startActivityForResult(CordovaPlugin arg0, Intent arg1, int arg2) {
+		// TODO Auto-generated method stub
+
 	}
+
+	
 
 }
